1f77d2d9c9f89a1ca6f852f9ad58612ab32ddf03
eldoor13 <andreas.siampanis@gmail.com>
2018-11-04 20:06:10 +0200

    Removed useless prints

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <time.h>
 #include "bucketChain.h"
+<<<<<<< HEAD
 #include "join.h"
 
 int main (int argc, char* argv[]){
@@ -12,6 +13,21 @@ int main (int argc, char* argv[]){
     struct bucket_array* buckets_table;
     struct psum* psum_table;
     int32_t* ordered_array;
+=======
+
+struct myTyple{
+  int32_t rowId;
+  int32_t value;
+};
+
+#define PRIME 73
+
+int main (int argc, char* argv[]){
+
+    int r=0, i=0, *bucket=NULL, *chain=NULL, tempI=0, h=-1;
+    struct myTyple *array=NULL;
+
+>>>>>>> Removed useless prints
   // if ( (argc!=5) && (argc!=3) ){
   //    printf("Number of arguments is wrong!!!\n");
   //    return 1;
@@ -34,8 +50,7 @@ int main (int argc, char* argv[]){
 
 
 
-  bucket=bucketCreate(PRIME);
-
+    bucket=bucketCreate(PRIME);
 
 
     chain=chainCreate(r);
@@ -44,6 +59,9 @@ int main (int argc, char* argv[]){
       printf("RowId: %d , Value: %d\n", array->tuples[i].rowId, array->tuples[i].value );
     }
     //asigning proper values for bucket and chain
+
+//asign proper values to chain and bucket
+
     for(i=0; i<r; i++){
         h=hash(array[i].value, PRIME);
         if(bucket[h]==-1){
@@ -66,15 +84,19 @@ int main (int argc, char* argv[]){
             }
             chain[tempI]=i;
             chain[i]=tempI;
-          }
-      }
+            //tempi=chain[bucket[h]
 
+          }
+
+        }
     }
     buckets_table = hash_data_array(array);
     psum_table = create_psum_table(buckets_table);
     ordered_array = create_ordered_data_array(r, psum_table, buckets_table);
 
     free(array->tuples);
-    free(array);//possible exception
+    free(chain);
+    free(bucket);
+    free(array);
 
 }
