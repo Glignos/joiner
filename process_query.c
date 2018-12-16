4d501bbb128e8dcea081ebf74843c47d196a6e00
Glignos <glignos93@gmail.com>
2018-12-16 16:46:10 +0200

    unfinished need supplementary functions
    Signed-off-by: Glignos <glignos93@gmail.com>

--- process_query.c
+++ process_query.c
@@ -25,32 +25,27 @@ void update_generated_table_mapping(struct generated_tables generated_tables, st
             free(generated_tables.tables[table1_replaced].table_pointer);
             generated_tables.tables[table1_replaced].table_pointer = newTable;//assign new and add new table to the used list
             generated_tables.tables[table1_replaced].num_of_tables++;
-            generated_tables.tables[table1_replaced].tables_used[generated_tables.tables[table1_replaced].num_of_tables] = comparison.table_pair_2.table;
+            generated_tables.tables[table1_replaced].tables_used[generated_tables.tables[table1_replaced].num_of_tables - 1] = comparison.table_pair_2.table;
             generated_tables.total_tables--;//we decrease since we have one less array
             for(int z=table2_replaced; z<generated_tables.total_tables;z++){
-                memcpy(&generated_tables.tables[z], &generated_tables.tables[z+1], sizeof(struct generated_table));
+                memcpy(&generated_tables.tables[z], &generated_tables.tables[z+1], sizeof(struct generated_table));//we remove the empty element from the array
             }
         }
-    else if(table1_replaced && comparison.number == NULL){
+    else if(table1_replaced){
             //free previous
             free(generated_tables.tables[table1_replaced].table_pointer);
             generated_tables.tables[table1_replaced].table_pointer = newTable;
-            generated_tables.num_of_tables[table1_replaced]++;
-            generated_tables.tables_used[table1_replaced][z+1] = comparison.table_pair_2.table;
+            if(comparison.number == NULL){//if we used a number no additional table is used
+            generated_tables.tables[table1_replaced].num_of_tables++;
+            generated_tables.tables[table1_replaced].tables_used[generated_tables.tables[table1_replaced].num_of_tables - 1] = comparison.table_pair_2.table;
+            }
         }
         else if(table2_replaced){
             //free previous
-            free(generated_tables.tables[table2_replaced].table_pointer);
-            generated_tables.tables[table2_replaced] = newTable;
-            for(int z=0;z<generated_tables.num_of_tables[table2_replaced];z++){//update used table
-                if(generated_tables.tables_used[table2_replaced][z] == comparison.table_pair_1.table){//add new table comprising the table we have now
-                break;//if its allready contained etc current table has been generated bt table 1 and 2 curr query is 2 and 3 new table should have 1 2 3
-                }
-                if(z == generated_tables.num_of_tables[table2_replaced] + 1){//add new table
-                    generated_tables.num_of_tables[table2_replaced]++;
-                    generated_tables.tables_used[table2_replaced][z+1] = comparison.table_pair_1.table;
-                }
-            }
+            free(generated_tables.tables[table2_replaced].table_pointer);//free previous generated table
+            generated_tables.tables[table2_replaced].table_pointer = newTable;//assign new
+            generated_tables.tables[table2_replaced].num_of_tables++;
+            generated_tables.tables[table2_replaced].tables_used[generated_tables.tables[table2_replaced].num_of_tables - 1] = comparison.table_pair_1.table;//replace and add new table to the tables used to produce the current one
         }
 }
 
@@ -71,7 +66,7 @@ void run_query(struct nMap* tables, struct query query, struct table* temp_table
     int table1_replaced, table2_replaced;
     struct nMap* table1_pointer;
     struct nMap* table2_pointer;
-    generated_tables.tables = malloc(query.comparisons_num*sizeof(struct generated_table));
+    generated_tables.tables = malloc(query.comparisons_num*sizeof(struct generated_table));//move the allocation to the main to avoid losing time at allocating memory preallocate big enough number and check at this point if its enough for this query
     for(int y=0; y< query.comparisons_num;y++){
         generated_tables.tables[y].tables_used = malloc(tables->total_tables*sizeof(int));//fixme multiply with the total amount of tables
         generated_tables.tables[y].num_of_tables = 0;
@@ -90,14 +85,12 @@ void run_query(struct nMap* tables, struct query query, struct table* temp_table
             for(int y=0; y<generated_tables.total_tables; y++){
                 for(int z=0; z<generated_tables.tables[y].num_of_tables; z++){
                     if(generated_tables.tables[y].tables_used[z] == query.comparisons[i].table_pair_1.table)//check if table has been used in a previous subquery
-                        //maybe find a better way at this point
                         {
                             //replace table 1 pointer
                             table1_pointer = generated_tables.tables[y].table_pointer;
                             table1_replaced = y;
                         }
                     if(query.comparisons[i].number == NULL && generated_tables.tables[y].tables_used[z] == query.comparisons[i].table_pair_2.table) //check if table has been used in a previous subquery
-                        //maybe find a better way at this point
                         {
                             //replace table 2 pointer
                             table2_pointer = generated_tables.tables[y].table_pointer; //fixme
@@ -105,12 +98,13 @@ void run_query(struct nMap* tables, struct query query, struct table* temp_table
                         }
                 }
             }
-        } //fix case for number and understand lookup on same array
+        } //fix lookup on same array
         if(query.comparisons[i].number == NULL && table1_replaced == -1 && table2_replaced == -1){
         newTable = check_temps(query.comparisons[i], temp_tables);// check if this query has run before
         }
         if(newTable != NULL){
-            //load new array from temps
+            //use newtable for the result
+            //load it in the produced tables** table1 table2 and newTable
         }
         else{
 
@@ -120,13 +114,10 @@ void run_query(struct nMap* tables, struct query query, struct table* temp_table
             //combine tables
             //newTable = newly generated table
         }
-        //replace with array mix management
-        //update existing temp table
-        
-        generated_tables.tables_used[i][0] =  query.comparisons[i].table_pair_1.table;
-        generated_tables.tables_used[i][1] =  query.comparisons[i].table_pair_2.table;
-        generated_tables.tables[i] = newTable;
+        update_generated_table_mapping(generated_tables, query.comparisons[i], table1_replaced, table2_replaced, newTable);
     }
+    //if more than 1 tables combine remaining arrays
+    //run check sums
 }
 
 
