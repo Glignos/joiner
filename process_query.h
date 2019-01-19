bcbff2af42a091ff48746d304497a48026b25a98
Andreas <andreas@Andreass-MacBook-Air.local>
2019-01-19 22:17:38 +0200

    Add create table from matches (filter and number)

--- process_query.h
+++ process_query.h
@@ -56,6 +56,8 @@ void crossjoin_tables(struct generated_table*, struct generated_table*);
 
 struct nMap* create_table_from_matches(struct result_buffer*, struct nMap*, struct nMap*);
 
+struct nMap *create_table_from_matches_filter(struct result_buffer*, struct nMap*);
+
 struct table* create_temp_tables(struct nMapArray*);
 
 struct result_buffer* run_radix(struct nColumns*, struct nColumns*, int, int);
